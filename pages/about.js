eaff3764fd3b8c3a94e6c4cf2b682083ddc1ceb2
Sprof22 <richmondelaigwu@gmail.com>
2023-05-02 00:45:07 +0100

    layout setting

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Footer from "../components/Footer"
+
+ 
+function About() {
+  return (
+    <h1 className="content">About</h1>
+  )
+}
+
+export default About
+
+About.getLayout = function PageLayout(page){
+    return (
+        <>
+            {page}
+            <Footer />
+        </>
+    )
+}
